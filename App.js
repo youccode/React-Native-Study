6e084ba5544ff5980bee631a5b39803bf7f59e12
youccode <woochan226544@snu.ac.kr>
2022-03-13 00:45:26 +0900

    Day 1

--- App.js
+++ App.js
@@ -1,22 +1,35 @@
 import { StatusBar } from "expo-status-bar";
+import react, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { View, Text } from "react-native-web";
+import { Image, Button, TextInput, ScrollView } from "react-native-web";
 
 export default function App() {
+  const [text, setText] = useState();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Text>Hello!</Text>
+      <Button
+        title={"Push"}
+        onPress={() => {
+          console.log("You pushed the Button!");
+        }}
+        color={"red"}
+      ></Button>
+
+      <Image source={"./favicon.png"} style={styles.default}></Image>
+      <TextInput onChangeText={setText} value={text} />
     </View>
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  default: {
+    backgroundColor: "red",
+  },
+});
